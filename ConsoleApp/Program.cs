77ddb17b01c294d31c06991781f0db107c533c7b
Григорій . <mcwertyge@gmail.com>
2025-06-12 21:14:14 +0300

    add features

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,21 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-//  {Console.Write("");
-//  {[var name = Console.ReadLine();
-//  {[{значення2} name = Convert.To{значення2}(Console.ReadLine())
-//Console.WriteLine($"{name}");     $ - Форматування, @ - Сприймати тільки за текст, \n - Перехід на нову строку(писати в "")
-//
-//  Синтаксиз умови:
-//
-//  if (условие)
-//  {
-        // "==" - Дорівнює, "!=" - Не дорівнює, "<"/">" - Меньше/Більше, "<="/">=" - Меньше дорівнює/Більше дорівнює
-//  }
-//  else if (другое_условие)
-//  {
-        // "++" - Збільшення на 1, "--" - Зменьшення на 1, "||" - Або, "&&" - І
-//  }
-//  else
-//  {
-        // 
-//  }
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int firstNumber = 0;
+int secondNumber = 1;
+decimal result = 0;
+
+Console.WriteLine("Вітаю в калькуляторі з одним оператором!\nВ цій програмі можна тільки ділити\n(ToT)/~~~");
+try
+{
+        Console.Write("Введіть 1 число:\n");
+        firstNumber = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Введіть 2 число:\n");
+        secondNumber = Convert.ToInt16(Console.ReadLine());
+}
+catch
+{
+        throw new Exception("Введіть доречний формат числа!");
+}
+if (secondNumber == 0)
+{
+        throw new Exception("Неможна ділити на 0!");
+}
+else
+{
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Результат ділення твого першого числа на друге: {result}\n(Результат заокруглюється)");
+}
